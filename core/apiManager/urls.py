b67a0e5a7b0980f9176aee436419d4e6cccdddcc
John Lathrop <lathrop1009@gmail.com>
2025-10-16 13:30:14 -0500

    Removed Movie Serializer References

--- core/apiManager/urls.py
+++ core/apiManager/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('health/', views.health, name='api-health'),
-    path('movies/', views.movies_list, name='api-movies'),
+    
 ]
--- core/apiManager/views.py
+++ core/apiManager/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import MovieSerializer
+
 
 
 @api_view(['GET'])
